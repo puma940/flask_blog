39f6ec01ab99fc59acda6091d11440a4662b8c2a
pumapuma <puma941@gmail.com>
2020-08-29 15:32:34 +0200

    commented option added

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,17 +17,23 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
+
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
 
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
+    from flaskblog.comments.routes import comments
     from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
     app.register_blueprint(users)
     app.register_blueprint(posts)
+    app.register_blueprint(comments)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+
+    with app.app_context():
+        db.create_all()
 
     return app
--- flaskblog/models.py
+++ flaskblog/models.py
@@ -15,6 +15,7 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20),  nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    comments = db.relationship('Comment', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
@@ -38,7 +39,17 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
+    comments = db.relationship('Comment', backref='article')
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_commented = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Comment('{self.content}', '{self.date_commented}')"
